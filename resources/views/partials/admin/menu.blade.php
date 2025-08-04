edb569e889d1d9de5e9a4b6c1a3f26d79b59eb70
Mr.R <cofounder@juvo.app>
2025-08-04 16:39:40 +0200

    Update menu.blade.php

--- resources/views/partials/admin/menu.blade.php
+++ resources/views/partials/admin/menu.blade.php
@@ -1490,17 +1490,7 @@
                 </li>
             </ul>
         @endif
-        @if (\Auth::user()->type == 'super admin')
-            <ul class="dash-navbar">
-                @if (Gate::check('manage super admin dashboard'))
-                    <li class="dash-item dash-hasmenu {{ Request::segment(1) == 'dashboard' ? ' active' : '' }}">
-                        <a href="{{ route('client.dashboard.view') }}" class="dash-link">
-                            <span class="dash-micon"><i class="ti ti-home"></i></span><span
-                                class="dash-mtext">{{ __('Dashboard') }}</span>
-                        </a>
-                    </li>
-                @endif
-
+      
 
                 @can('manage user')
                     <li
