105da45ff29ff44b817f2e8f6cd0d242f467b54b
Mr.R <54802466+RendaniSinyage@users.noreply.github.com>
2025-08-16 11:37:21 +0200

    locale revert

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,10 +5,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Laravel\Socialite\Facades\Socialite;
-use App\Models\Utility;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -50,37 +46,5 @@ class AppServiceProvider extends ServiceProvider
                 $config['redirect']
             );
         });
-
-        if (!$this->app->runningInConsole()) {
-
-            $supportedLocales = ['en', 'za-en', 'en-ke'];
-            $locale = 'en'; // Default locale
-
-            // 1. Check for locale in the URL first
-            $urlLocale = request()->segment(1);
-            if (in_array($urlLocale, $supportedLocales)) {
-                $locale = $urlLocale;
-            }
-            // 2. If no locale in URL, check user settings
-            else if (Auth::check()) {
-                $user = Auth::user();
-                $settings = Utility::settingsById($user->id);
-
-                $currency = $settings['site_currency'] ?? 'USD';
-                $country = $settings['company_country'] ?? 'USA';
-
-                $mapping = [
-                    'ZAR' => ['South Africa' => 'za-en'],
-                    'KES' => ['Kenya' => 'en-ke'],
-                ];
-
-                if (isset($mapping[$currency][$country])) {
-                    $locale = $mapping[$currency][$country];
-                }
-            }
-
-            App::setLocale($locale);
-            URL::defaults(['locale' => $locale]);
-        }
     }
 }
